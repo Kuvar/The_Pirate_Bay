7b872655f4ff2e95bfe3efc630480f90d4d1349f
Kuvar Singh <kuvarjava@gmail.com>
2018-12-23 17:39:00 +0530

    ThePirateBey
    ThePirateBey is a .NET API to make torrent searches in The Pirate Bay website.

---
+++ The_Pirate_Bay/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Pirate_Bay
+{
+    public class Constants
+    {
+        public static string UrlSearch = "{0}/search/{1}/{2}/{3}/{4}";
+        public static string[] UrlTpb = new string[] { "http://pirateproxy.pl", "http://thepiratebay.sx"};
+    }
+}
